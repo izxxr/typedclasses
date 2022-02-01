a66939daa1ea673ce5528eec095dc44d23460f93
NerdGuyAhmad <thisisizharahmad@gmail.com>
2022-02-01 06:12:04 +0500

    Move validators to core file

--- typedclass/core.py
+++ typedclass/core.py
@@ -3,7 +3,7 @@
 
 from __future__ import annotations
 
-from typedclass import helpers
+import inspect
 import typing
 
 TC = typing.TypeVar("TC", bound="TypedClass")
@@ -25,7 +25,7 @@ class TypedClass:
             raise TypeError(f"{cls.__name__}() takes no positional arguments.")
 
         instance = super().__new__(cls)
-        return helpers.prepare_typed_instance(instance, kwargs)
+        return prepare_typed_instance(instance, kwargs)
 
     def __init_subclass__(cls, ignore_internal: bool = True) -> None:
         cls.__tc_options__ = options = dict()
@@ -63,3 +63,99 @@ class TypedClass:
         # Update the options with new ones.
         options["optional_params"] = optional_params
         options["required_params"] = required_params
+
+
+def apply_from_typing_origin(origin, tc: TypedClass, val: typing.Any, tp: type, name: str):
+    args = typing.get_args(tp)
+
+    if origin is typing.Union and args[1] is type(None) and len(args) == 2:
+        # typing.Optional[tp] is used which is internally taken
+        # as typing.Union[tp, None] so args[0] would be our required type.
+
+        if val is not None and not isinstance(val, args[0]):
+            raise TypeError(f"Parameter {name!r} in {tc.__class__.__name__}() must be None or " \
+                            f"{args[0]!r}, Not {val.__class__!r}")
+
+        return setattr(tc, name, val)
+
+    elif origin is typing.Union:
+        for arg in args:
+            origin = typing.get_origin(arg)
+
+            if origin is not None:
+                try:
+                    apply_from_typing_origin(origin, tc, val, arg, name)
+                except TypeError:
+                    continue
+                else:
+                    return
+
+            if isinstance(val, arg):
+                setattr(tc, name, val)
+                return
+
+        raise TypeError(f"Parameter {name!r} in {tc.__class__.__name__}() must be an instance " \
+                        f"of one of {', '.join(repr(arg) for arg in args)}, Not {val.__class__!r}")
+
+    elif origin is typing.Literal:
+        for arg in args:
+            if val is arg:
+                setattr(tc, name, tp)
+                return
+
+        raise TypeError(f"Parameter {name!r} in {tc.__class__.__name__}() must be exactly one " \
+                        f"of {', '.join(repr(arg) for arg in args)}, Not {val!r}")
+
+    elif origin is type:
+        if not inspect.isclass(val) or not issubclass(val, args[0]):
+            raise TypeError(f"Parameter {name!r} in {tc.__class__.__name__}() must be a type "
+                            f"instance of {args[0]!r}, Not {val!r}")
+
+        setattr(tc, name, val)
+
+def apply_attr(tc: TypedClass, val: typing.Any, tp: type, name: str):
+    origin = typing.get_origin(tp)
+
+    if origin is not None:
+        apply_from_typing_origin(origin, tc, val, tp, name)
+        return
+
+    if not isinstance(val, tp):
+        raise TypeError(f"Parameter {name!r} in {tc.__class__.__name__}() must be an " \
+                        f"instance of {tp!r}, Not {val.__class__!r}")
+
+    setattr(tc, name, val)
+
+def prepare_typed_instance(tc: TC, params: typing.Dict[str, typing.Any]) -> TC:
+    options = tc.__tc_options__
+
+    required_params = options["required_params"]
+    optional_params = options["optional_params"]
+
+    missing_params = []
+
+    for p_name, p_type in required_params.items():
+        if not p_name in params:
+            missing_params.append(p_name)
+            continue
+
+        apply_attr(tc, params[p_name], p_type, p_name)
+        params.pop(p_name)
+
+
+    if missing_params:
+        raise TypeError(f"{tc.__class__.__name__}() is missing required parameters " \
+                        f"{', '.join(repr(p) for p in missing_params)}")
+
+    for p_name, p_type in optional_params.items():
+        if not p_name in params:
+            continue
+
+        apply_attr(tc, params[p_name], p_type, p_name)
+        params.pop(p_name)
+
+    if params:
+        raise TypeError(f"{tc.__class__.__name__}() got unexpected parameters " \
+                        f"{', '.join(repr(p) for p in params)}")
+
+    return tc
