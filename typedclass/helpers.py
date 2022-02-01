219a711888594a866c1b21ba6bd36e109ac4356d
NerdGuyAhmad <thisisizharahmad@gmail.com>
2022-02-01 06:10:07 +0500

    Add support for typing.Literal and Type

--- typedclass/helpers.py
+++ typedclass/helpers.py
@@ -3,6 +3,7 @@
 
 from __future__ import annotations
 
+import inspect
 import typing
 
 if typing.TYPE_CHECKING:
@@ -41,6 +42,22 @@ def apply_from_typing_origin(origin, tc: TypedClass, val: typing.Any, tp: type, 
 
         raise TypeError(f"Parameter {name!r} in {tc.__class__.__name__}() must be an instance " \
                         f"of one of {', '.join(repr(arg) for arg in args)}, Not {val.__class__!r}")
+
+    elif origin is typing.Literal:
+        for arg in args:
+            if val is arg:
+                setattr(tc, name, tp)
+                return
+
+        raise TypeError(f"Parameter {name!r} in {tc.__class__.__name__}() must be exactly one " \
+                        f"of {', '.join(repr(arg) for arg in args)}, Not {val!r}")
+
+    elif origin is type:
+        if not inspect.isclass(val) or not issubclass(val, args[0]):
+            raise TypeError(f"Parameter {name!r} in {tc.__class__.__name__}() must be a type "
+                            f"instance of {args[0]!r}, Not {val!r}")
+
+        setattr(tc, name, val)
 
 def apply_attr(tc: TypedClass, val: typing.Any, tp: type, name: str):
     origin = typing.get_origin(tp)
