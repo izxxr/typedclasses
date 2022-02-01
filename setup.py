03464ed217d1e63bbba2de602447af0d85b81b29
NerdGuyAhmad <thisisizharahmad@gmail.com>
2022-02-01 06:28:18 +0500

    Add PyPi metadata

---
+++ setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup
+
+
+with open("README.MD", "r") as f:
+    LONG_DESCRIPTION = f.read()
+
+VERSION = "0.0.1"
+GITHUB = "https://github.com/nerdguyahmad/typed-class"
+LICENSE = "MIT"
+REQUIREMENTS = []
+PACKAGES = ["typedclass"]
+
+setup(
+    name="typed-class",
+    author="nerdguyahmad",
+    version=VERSION,
+    license=LICENSE,
+    url=GITHUB,
+    project_urls={
+        "Documentation": GITHUB + "/wiki",
+        "Issue tracker": GITHUB + "/issues",
+    },
+    description='Python classes with types validation at runtime.',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    install_requires=REQUIREMENTS,
+    packages=PACKAGES,
+    python_requires='>=3.8.0',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+        'Typing :: Typed',
+    ]
+)
